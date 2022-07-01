6a027100e5ffec219080a5c7a62fc5881357dee1
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-01 19:29:00 +0600

    #2

--- cicle.py
+++ cicle.py
@@ -1,6 +1,17 @@
+#1
+from ast import While
+
+
 lang1 = "Rust"
 languages = ['go', 'java', 'php', 'python', 'ruby', 'javascript']
 
 for i in languages:
     if i == lang1:
         print("This Languages is in list")
+
+#2
+languages = ['go', 'java', 'php', 'python', 'ruby', 'javascript']
+for i in languages:
+    print(i)
+    if i == 'php':
+        break
