e6c99a5671b8612ba130f4b525b541825c8ff1ed
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-04 19:19:59 +0600

    dict 2 ex

--- dict.py
+++ dict.py
@@ -1,4 +1,13 @@
 #1
-menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
-menu['drinks'] = 'Coca-Cola', 'Sprite', 'Fanta'
-print(menu)
+# menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
+# menu['drinks'] = 'Coca-Cola', 'Sprite', 'Fanta'
+# print(menu)
+
+#2
+
+for x in range(3):
+    info = {}
+    name = input('Введите свое имя ')
+    password = input('Введите свой пароль ')  
+    info[name] = password
+    print(info)
