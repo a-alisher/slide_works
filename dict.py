cb9cb538bc4b0683fc693f47e99a780502ce0d25
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-04 19:32:26 +0600

    done 3ex dict

--- dict.py
+++ dict.py
@@ -5,9 +5,18 @@
 
 #2
 
-for x in range(3):
-    info = {}
-    name = input('Введите свое имя ')
-    password = input('Введите свой пароль ')  
-    info[name] = password
-    print(info)
+# for x in range(3):
+#     info = {}
+#     name = input('Введите свое имя ')
+#     password = input('Введите свой пароль ')  
+#     info[name] = password
+#     print(info)
+
+#3
+for x in range(5):
+    spec = {}
+    name_user = input('Введите свое имя ')
+    speciality = input('Введите свою Профессию ')
+    spec[name_user] = speciality
+    print(f"Здравствуйте, {name_user}. У Вас прекрасная профессия - {speciality}")
+    
