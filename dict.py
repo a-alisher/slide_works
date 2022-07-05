ceb0fc7f95a438abf9aa66023000e77c93881ca4
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-05 18:52:10 +0600

    done 5 ex

--- dict.py
+++ dict.py
@@ -21,8 +21,18 @@
 #     print(f"Здравствуйте, {name_user}. У Вас прекрасная профессия - {speciality}")
 
 #4
-menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
-menu['besh-barmak'] = 130
-menu['lagman'] = 135
-del menu['borsh']
-print(menu)
+# menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
+# menu['besh-barmak'] = 130
+# menu['lagman'] = 135
+# del menu['borsh']
+# print(menu)
+
+#5
+
+south_american_countries = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Suriname', 'Uruguay', 'Venezuela']
+cnt = 1
+country = {}
+for i in set(south_american_countries):
+    country[cnt] = i
+    cnt += 1
+print(country)
