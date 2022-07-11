feaf509a75d39846fdff8c424e9d0759907091ab
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-11 20:00:15 +0600

    done 2 exercise

--- exceptions.py
+++ exceptions.py
@@ -1,17 +1,36 @@
-try:
-    k = "bla-bla" + 27
-    print("Вы ввели: ", k)
-except TypeError as te:
-    print("Type_Error")
+# try:
+#     k = "bla-bla" + 27
+#     print("Вы ввели: ", k)
+# except TypeError as te:
+#     print("Type_Error")
+#
+# try:
+#     k = "blabla"
+#     print(k[10])
+# except IndexError as ie:
+#     print("Index Error")
+# try:
+#     a = 1
+#     k = a + b
+#     print(k)
+# except NameError:
+#     print("NameError")
 
+# 2 exercise
+
+at = 10
+i = 15
+wo = 20
 try:
-    k = "blabla"
-    print(k[10])
-except IndexError as ie:
-    print("Index Error")
-try:
-    a = 1
-    k = a + b
-    print(k)
+    for e in range(-at, at):
+        print(wo / e)
+        if at > 5:
+            print(int("at" > '5'))
+except IndentationError:
+    print("IndentationError")
 except NameError:
     print("NameError")
+except SyntaxError:
+    print("syntaxerror")
+except ZeroDivisionError:
+    print("ZERO Error")
