750725b24fc87dfd08f4ab533b42ce782d90e0c5
Alisher Alimkanov <alisheralimkanov@MacBook-Air-Alisher.local>
2022-07-13 13:35:15 +0600

    done 1 exercise

--- functions.py
+++ functions.py
@@ -6,7 +6,13 @@
 # send_email('Mirbek', 8)
 
 l = [0, 1, 2, 3]
+
+
 def reverse_list(list):
+    # length = len(list)
+    # middle_index = length % 2
+    # # first_half = list[middle_index:-1]
+    # # second_half = list[middle_index:-1]
     l1 = l[1::-1]
     l2 = l[:-3:-1]
     print(l1 + l2)
